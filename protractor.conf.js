e4c07dd8d38b113460dacd2c50de081711561970
Alexey Alexeev <aaliakseyeu@exadel.com>
2016-06-06 15:53:25 +0300

    Try to run selenium tests on SL server

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,13 +1,63 @@
-module.exports = {
-    config: {
-        specs: ['**/*.e2e.spec.js'],
-        exclude: ['node_modules/**/*'],
-        baseUrl: 'http://localhost:8080',
-        seleniumAddress: 'http://localhost:4444/wd/hub',
-        capabilities: {
-            'browserName': 'phantomjs',
-            'phantomjs.binary.path': require('phantomjs').path,
-            'phantomjs.ghostdriver.cli.args': ['--loglevel=ERROR']
-        }
-    }
+exports.config = {
+	sauceUser: process.env.SAUCE_USERNAME,
+	sauceKey: process.env.SAUCE_ACCESS_KEY,
+	specs: ['**/*.e2e.spec.js'],
+	exclude: ['node_modules/**/*'],
+	baseUrl: 'http://localhost:8080',
+	jasmineNodeOpts: {
+		showColors: true,
+		isVerbose: true,
+		realtimeFailure: true,
+		includeStackTrace: true,
+		defaultTimeoutInterval: 30000
+	},
+	multiCapabilities: [{
+		'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+		build: process.env.TRAVIS_BUILD_NUMBER,
+		name: 'ng-pattern-restrict Chrome build ' + process.env.TRAVIS_BUILD_NUMBER,
+		browserName: 'chrome',
+		shardTestFiles: true,
+		maxInstances: 5,
+		seleniumVersion: '2.46.0'
+	}/*, {
+	 'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+	 build: process.env.TRAVIS_BUILD_NUMBER,
+	 name: 'ng-pattern-restrict Firefox build ' + process.env.TRAVIS_BUILD_NUMBER,
+	 browserName: 'firefox',
+	 shardTestFiles: true,
+	 maxInstances: 5,
+	 seleniumVersion: '2.46.0'
+	 }, {
+	 'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+	 build: process.env.TRAVIS_BUILD_NUMBER,
+	 name: 'ng-pattern-restrict IE build ' + process.env.TRAVIS_BUILD_NUMBER,
+	 browserName: 'internet explorer',
+	 shardTestFiles: true,
+	 maxInstances: 5,
+	 seleniumVersion: '2.46.0'
+	 }, {
+	 'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+	 build: process.env.TRAVIS_BUILD_NUMBER,
+	 name: 'ng-pattern-restrict Safari build ' + process.env.TRAVIS_BUILD_NUMBER,
+	 browserName: 'safari',
+	 shardTestFiles: true,
+	 maxInstances: 5,
+	 seleniumVersion: '2.46.0'
+	 }, {
+	 'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+	 build: process.env.TRAVIS_BUILD_NUMBER,
+	 name: 'ng-pattern-restrict iOS build ' + process.env.TRAVIS_BUILD_NUMBER,
+	 browserName: 'iphone',
+	 shardTestFiles: true,
+	 maxInstances: 5,
+	 seleniumVersion: '2.46.0'
+	 }, {
+	 'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+	 build: process.env.TRAVIS_BUILD_NUMBER,
+	 name: 'ng-pattern-restrict Android build ' + process.env.TRAVIS_BUILD_NUMBER,
+	 browserName: 'android',
+	 shardTestFiles: true,
+	 maxInstances: 5,
+	 seleniumVersion: '2.46.0'
+	 }*/]
 };
